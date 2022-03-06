b49fdd41c3fbe9c0fefdb8003be1e5e6ce848376
Naveen Agarwal <naveenagarwal123451@gmail.com>
2022-03-06 20:00:37 +0530

    add options for alpha it self

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -9,21 +9,25 @@ M.options = {
     clipboard = "unnamedplus",
     cmdheight = 1,
     cursorline = true, -- recommended to not change
+
     -- Indentline
     expandtab = true,
     shiftwidth = 2,
     smartindent = true,
+
     -- disable tilde on end of buffer
     fillchars = {eob = " "},
     hidden = true,
     ignorecase = true,
     smartcase = true,
     mouse = "a",
+
     -- Numbers
     number = true,
     numberwidth = 2,
     relativenumber = true,
     ruler = false,
+
     signcolumn = "yes", -- recommended to not change
     splitbelow = true, -- recommended to not change
     splitright = true, -- recommended to not change
@@ -31,12 +35,25 @@ M.options = {
     termguicolors = true, -- recommended to not change
     timeoutlen = 400,
     undofile = true,
+
     -- interval for writing swap file to disk, also used by gitsigns
     updatetime = 250,
-    
-    shadafile = "NONE",
+
+    shadafile = "NONE"
+  },
+  
+  -- alpha options
+  alpha {
+    copy_cut = true, -- copy cut text ( x key ), visual and normal mode
+    copy_del = true, -- copy deleted text ( dd key ), visual and normal mode
+    insert_nav = true, -- navigation in insertmode
+    window_nav = true,
+    terminal_numbers = false,
+
+    -- updater
+    update_url = "https://github.com/aditya612/AlphaNvim",
+    update_branch = "main",
   }
 }
-
 
 return M
